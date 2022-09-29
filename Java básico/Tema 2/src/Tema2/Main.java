e7f5e028231716a4aed3659b992e9410ff5ee7b7
Camila Pinzon <camilapinz18@gmail.com>
2022-09-29 12:39:49 -0500

    Tema 2

--- Java básico/Tema 2/src/Tema2/Main.java
+++ Java básico/Tema 2/src/Tema2/Main.java
@@ -5,22 +5,17 @@ public class Main {
         //Para este ejercicio tendréis que crear una
         // función que reciba un precio y devuelva el precio con el IVA incluido.
 
-        float precio=0;
-        float iva=0;
-        float precioIva;
-
-        precio=56.23f;
+        float precio=165.25F;
         System.out.println("El precio del producto sin IVA es de: "+precio);
 
-        iva=anadirIva(precio);
-        precioIva=precio+iva;
-        System.out.println("El precio del producto con iva es de: "+precioIva);
+        float precioIva=anadirIva(precio);
+        System.out.println("El precio del producto con IVA es de: "+precioIva);
     }
 
     public static float anadirIva(float precio)
     {
         float iva=0.19f;
         float precioConIva=precio*iva;
-        return precioConIva;
+        return precio+precioConIva;
     }
 }
